44381f9f13e3bb7c247116059246f944ff854132
mangoshoieb <159851387+mangoshoieb@users.noreply.github.com>
2025-05-05 00:11:38 +0300

    redirect

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
-import {layout, route, type RouteConfig,} from "@react-router/dev/routes";
+import {layout, route, type RouteConfig} from "@react-router/dev/routes";
 
 export default [
+    redirect("/", "/dashboard"),
     layout("routes/admin/admin-layout.tsx", [
         route("dashboard", "routes/admin/dashboard.tsx"),
         route("all-users", "routes/admin/all-users.tsx"),
